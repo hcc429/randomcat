abacfe96a4c1b48c12560b0beecb695e2edc8dba
hcc429 <thomas04298@gmail.com>
2023-06-10 18:41:59 +0800

    add deploy.yml for automatically building and deploying the images

--- load.js
+++ load.js
@@ -1,13 +1,29 @@
 import http from "k6/http";
 import { sleep } from "k6";
+import { check } from 'k6';
+import { Rate, Trend } from "k6/metrics";
+//docker run --rm -i grafana/k6 run - <load.js
 
+// Define custom metrics
+//const failedRequests = new Rate("failed_requests");
+//const requestsPerSecond = new Trend("requests_per_second");
 
-
-export default () => {
-  http.get("http://host.docker.internal:8080/image/?w=100&h=170");
-  // MORE STEPS
-  // Here you can have more steps or complex script
-  // Step1
-  // Step2
-  // etc.
+export const options = {
+  stages: [
+    { duration: '10s', target: 200 },
+    { duration: '100s', target: 200 },
+    { duration: '10s', target: 100}
+  ],
 };
+// export const options = {
+//   vus: 2000,
+//   duration: '10s',
+//   rps: 120
+// };
+export const sameImage = false
+export default function () {
+  const url = "https://randomcat.io/api/image?w=100&h=170";
+  const res = http.get(url);
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  //sleep(1);
+}
