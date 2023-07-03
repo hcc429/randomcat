e7e1b2ae09e3b7a95dd343b677d6210722e66d6c
YU-TING HUANG <b07703118@ntu.edu.tw>
2023-07-03 11:32:00 +0800

    test ci/cd

--- server/controllers/dto/like.go
+++ server/controllers/dto/like.go
@@ -1,10 +1,10 @@
 package dto
 
-type Likes struct{
+type Likes struct {
 	Likes []Like `binding:"required"`
 }
 
-type Like struct{
-	URL string  `json:"url"`
-	Amount int  `json:"amount"`
+type Like struct {
+	URL    string `json:"url"`
+	Amount int    `json:"amount"`
 }
