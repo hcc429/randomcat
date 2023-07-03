b91fc39e4f33753d8faa03ebb8584c827eb49d18
YU-TING HUANG <b07703118@ntu.edu.tw>
2023-07-03 11:54:41 +0800

    test ci/cd

--- server/router/metric.go
+++ server/router/metric.go
@@ -4,14 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
-	
 
-func promHandler() gin.HandlerFunc{
-	return func(c *gin.Context){
+func promHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		promhttp.Handler().ServeHTTP(c.Writer, c.Request)
 	}
 }
 
-func AddMetricRoute(r *gin.Engine){
+func AddMetricRoute(r *gin.Engine) {
 	r.GET("/metrics", promHandler())
 }
